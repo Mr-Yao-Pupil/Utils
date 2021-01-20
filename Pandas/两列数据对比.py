c484a950f4a282a8d880501df555a12232470980
Mr-Yao-Pupil <yaozhichao45@gmail.com>
2021-01-20 16:16:34 +0800

    上传关于pandas中Dataframe两列数据对比的代码

--- Pandas/两列数据对比.py
+++ Pandas/两列数据对比.py
@@ -17,9 +17,9 @@ def del_diffrient_series(df, columns_name1, columns_name2):
     :return: 删除两列索引的不同行后的dataframe
     """
     if columns_name1 not in df.columns.tolist():
-        raise ValueError(f"{columns_name1}不是输入Dataframe种的列索引")
+        raise ValueError(f"{columns_name1}不是输入Dataframe中的列索引")
     if columns_name2 not in df.columns.tolist():
-        raise ValueError(f"{columns_name2}不是输入Dataframe种的列索引")
+        raise ValueError(f"{columns_name2}不是输入Dataframe中的列索引")
     if not isinstance(df, pd.DataFrame):
         raise TypeError(f"参数df必须输入pandas.DataFrame的实例对象")
     df = df[df.apply(lambda x: function_same(x[columns_name1], x[columns_name2]), axis=1)]
