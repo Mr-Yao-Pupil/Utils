9645eae8d993d84ecc7f33a4273f0b9239c12ca7
Mr-Yao-Pupil <yaozhichao45@gmail.com>
2021-03-26 14:43:40 +0800

    提交图像膨胀和腐蚀的代码

---
+++ OpenCV/image_dilate_and_erode.py
@@ -0,0 +1,13 @@
+import cv2
+
+
+def image_dilate(image, kernel_size=5):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+    return cv2.dilate(image, kernel=kernel)
+
+
+def image_erode(image, kernel_size=3):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
+    return cv2.dilate(image, kernel)
+
+
