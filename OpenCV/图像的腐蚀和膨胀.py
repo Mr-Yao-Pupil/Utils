ebd3eaedcf85e708a039b891de226c16643d5e76
Mr-Yao-Pupil <yaozhichao45@gmail.com>
2021-04-02 19:44:16 +0800

    更新腐蚀代码

--- OpenCV/图像的腐蚀和膨胀.py
+++ OpenCV/图像的腐蚀和膨胀.py
@@ -8,6 +8,6 @@ def image_dilate(image, kernel_size=5):
 
 def image_erode(image, kernel_size=3):
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
-    return cv2.dilate(image, kernel)
+    return cv2.erode(image, kernel)
 
 
